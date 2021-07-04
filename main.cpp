80e4325c4a3cf513a59dcd2f8c0456887d91a300
russell2440 <85845760+russell2440@users.noreply.github.com>
2021-07-04 21:13:35 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <exception>
+
+using namespace std;
+
+class my_exception: public exception{
+  public:
+  virtual const char *what() const throw(){
+    return "Something BAADD happened!!";
+  }
+};
+
+class test{
+  public:
+    void goes_wrong()
+    {
+      throw my_exception();
+    }
+};
+
+int main() {
+   cout << __PRETTY_FUNCTION__<< ": Hello World!\n";
+   test tst;
+   try {
+     tst.goes_wrong();
+    }
+    catch (my_exception &e)
+    {
+        cout<<e.what()<<endl;
+    }
+    cout<< __PRETTY_FUNCTION__<<": still running\n"<<endl;
+    return 0;
+}
